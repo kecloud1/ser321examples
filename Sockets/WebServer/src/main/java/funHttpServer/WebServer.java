7cdffc9e6d0663281921c49346bee5ec40e103c5
Kelsey Rendon <kelseycloud@Kelseys-MacBook-Pro.local>
2023-01-22 13:37:34 -0700

    Fix line 381

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -378,7 +378,7 @@ class WebServer {
               builder.append("dogID needs to be a number between 1 and 5. Please try again.");
             }
             else {
-              int numTreats = Integer.parseInt(query_pairs.get("numOfTreats"));
+              int numTreats = Integer.parseInt(query_pairs.get("numTreats"));
 
               int dogID = Integer.parseInt(query_pairs.get("dogID")) - 1;
 
