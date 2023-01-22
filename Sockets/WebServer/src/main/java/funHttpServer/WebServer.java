cb1ad6380c144deccb129c720752b537fba562e3
Kelsey Rendon <kelseycloud@Kelseys-MacBook-Pro.local>
2023-01-22 11:42:47 -0700

    Update error message

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -258,7 +258,7 @@ class WebServer {
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
-            builder.append("User not found.");
+            builder.append("User not found or request not formatted correctly.");
           }
           else {
             builder.append("HTTP/1.1 200 OK\n");
